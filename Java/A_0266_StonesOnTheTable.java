0e1df8cece62372a9355e683e7dfdecce5dfb5ce
Ayoub Droubi <ayoub.al.droubi@gmail.com>
2024-07-10 12:29:56 +0300

    Create A_0266_StonesOnTheTable.java

---
+++ Java/A_0266_StonesOnTheTable.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ayyoub
+ */
+public class Stones_on_the_Table {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args) throws java.io.IOException
+	{
+		// Input
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String s = sc.next();
+
+		// Counters
+		int counter = 0;
+		char last = s.charAt(0);
+
+		for (int i = 1; i < n; i++) {
+			if (s.charAt(i) == last) counter++;
+			else last = s.charAt(i);
+		}
+
+		// Output
+		System.out.println(counter);
+	}	
+}
